b48273ee3b84eb372a8ab002ff7886490c910e6c
will <1522188485@qq.com>
2018-06-10 11:59:54 +0800

    新建

--- store/index.js
+++ store/index.js
@@ -1,21 +1,3 @@
-// import {createStore} from 'redux';
-// import todoApp from '../reducers';
-//
-// let cacheTodos  = localStorage.getItem('todos');
-// try{
-//     cacheTodos = JSON.parse(cacheTodos)
-// }catch(e){
-//     cacheTodos = {};
-// }finally {
-//     cacheTodos = cacheTodos || {};
-// }
-//
-//
-// let store = createStore(
-//     todoApp
-//     ,cacheTodos
-//     ,window.devToolsExtension ? window.devToolsExtension() : undefined//Chrome Extension
-// );
 import { createStore, applyMiddleware ,compose} from 'redux';
 import thunk from 'redux-thunk';
 
