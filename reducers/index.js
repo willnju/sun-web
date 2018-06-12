7924774598fb6af8e23f5198226ad6e2a6900ed5
Stephen <mengfansai147@163.com>
2018-06-12 09:55:32 +0800

    admin

--- reducers/index.js
+++ reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {homeBanner} from './home';
+import { reducer as formReducer } from 'redux-form';
 
 const todoApp = combineReducers({
     homeBanner:homeBanner,
+    form:formReducer,
 });
 
 export default todoApp;
